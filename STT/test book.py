b138a9971ff8a613db09720152ec0d4dfd61dc32
Fsnakcsk <fsnakcsk@naver.com>
2023-02-05 18:20:53 +0900

    u

--- STT/test book.py
+++ STT/test book.py
@@ -1,16 +1,22 @@
 from flask import Flask, flash, redirect, render_template, request, url_for, jsonify
 import sqlite3 as sql
+<<<<<<< Updated upstream
 from flask import g
 import os
 
+=======
+>>>>>>> Stashed changes
 import base64
-import requests
 from time import sleep
 
 import urllib3
 import json
+<<<<<<< Updated upstream
 
 import boto3
+=======
+from count import count
+>>>>>>> Stashed changes
 
 app = Flask(__name__)
 count = 1
