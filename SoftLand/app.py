5f42a7bf36559d276cb1c1d0d6bb27b181995f57
Fsnakcsk <fsnakcsk@naver.com>
2023-02-07 17:17:01 +0900

    u

--- SoftLand/app.py
+++ SoftLand/app.py
@@ -385,20 +385,18 @@ def STT():
     #---------------------------------------------------------------------------
     if request.method == 'POST':
         
-        data = request.files['files'].read()
-        
-        # blob = request.data
+        #blob = request.data
         # Do something with the blob
 
         
         openApiURL = "http://aiopen.etri.re.kr:8000/WiseASR/Recognition"
         accessKey = "f0f9fd15-daef-4655-b516-d7a9711c696a" 
-        # audioFilePath = "C:/Users/admin/Downloads/정답1.wav" # 다운로드한 음성파일을 여기에 넣어서 Text로 바꾸기
-        # audioFilePath = data
+        audioFilePath = "C:\\Users\\admin\\Desktop\\정답1.wav" # 다운로드한 음성파일을 여기에 넣어서 Text로 바꾸기
+        #audioFilePath = blob
         languageCode = "korean"
         
         # file = open(audioFilePath, "rb")
-        file = open(data, "rb")
+        file = open(audioFilePath, "rb")
         audioContents = base64.b64encode(file.read()).decode("utf8")
         file.close()
         
